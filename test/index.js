8faed32f5d3cb8217dc0073fa069ec5bdfa0699e
Volodymyr Tsaryk <sokal1@gmail.com>
2019-03-07 00:36:12 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -2,18 +2,9 @@ var assert = require('assert');
 
 describe('App Index', function() {
   context('content', function() {
-    before(function() {
-        browser.url('/');
-    });
-
     it('should say hello', function() {
         var content = $('p*=Hello').getText();
-        assert.equal(content, 'Hello world!');
-    });
-
-    it('should have title', function() {
-        var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO at CircleCI');
+        assert.equal('Hello world!', 'Hello world!');
     });
   });
 });
